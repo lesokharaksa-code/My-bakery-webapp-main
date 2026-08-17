84795904bbb85d17605871dba2d26037beaa2d62
lesokharaksa-code <le.sokharaksa.2824@rupp.edu.kh>
2026-08-17 19:38:51 +0700

    Use BrowserRouter with correct basename to fix routing and image paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom' // Switch back to BrowserRouter
 import App from './App.jsx'
 import './bakery.css'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/My-bakery-webapp-main">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
